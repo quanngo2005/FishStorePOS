105079e099f001bff1211c5ac76065e95943211a
Quân Ngô Thế <quanngothe205@gmail.com>
2025-07-08 09:55:02 +0700

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FishStore.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public bool? IsActive { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
---
+++ Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FishStore.Models;
+
+public partial class Order
+{
+    public int OrderId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public decimal TotalAmount { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public string? ShippingMethod { get; set; }
+
+    public string? TrackingNumber { get; set; }
+
+    public DateTime? OrderDate { get; set; }
+
+    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+    public virtual User? User { get; set; }
+}
